446cd5db5c22cb451d76b6276984c8af2bf409c5
Durgut <nazifcan.durgut@puretechnology.com.tr>
2024-06-01 05:41:54 +0300

    add page title

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,6 +4,10 @@ import LastPosts from '../components/LastPosts';
 import Repositories from '../components/Repositories';
 import YoutubeVideos from '../components/YoutubeVideos';
 
+export const metadata = {
+  title: 'Nazif Can DURGUT',
+};
+
 const Home = async () => {
   return (
     <div className="container mt-12 flex flex-col gap-12 pb-48">
--- app/components/Me/index.tsx
+++ app/components/Me/index.tsx
@@ -37,44 +37,49 @@ const SOCIAL_MEDIAS = [
 
 const Me = () => {
   return (
-    <div className="w-full min-h-[100px] flex flex-col">
-      <div className="flex flex-col">
-        <div className="flex items-center justify-between">
-          <div>
-            <h1 className="text-black font-bold text-4xl">Nazif Can DURGUT</h1>
-            <span>a fullstack developer based in Turkey.</span>
+    <>
+      <div className="w-full min-h-[100px] flex flex-col">
+        <div className="flex flex-col">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-black font-bold text-4xl">
+                Nazif Can DURGUT
+              </h1>
+              <span>a fullstack developer based in Turkey.</span>
+            </div>
+
+            <Image
+              src={'/me.jpg'}
+              alt="Nazif Can DURGUT"
+              width={75}
+              height={75}
+              className="rounded-xl w-[75px] h-[75px]"
+            />
           </div>
 
-          <Image
-            src={'/me.jpg'}
-            alt="Nazif Can DURGUT"
-            width={75}
-            height={75}
-            className="rounded-xl w-[75px] h-[75px]"
-          />
-        </div>
+          <p className="text-lg mt-4">
+            Hello, I&lsquo;m Nazif. I love working with Javascript technologies.
+            If you have a project, I would like to help you. You can contact me
+            at{' '}
+            <Link
+              href="mailto:nazifcandurgut@icloud.com"
+              className="text-black font-medium hover:text-purple-600"
+            >
+              nazifcandurgut@icloud.com
+            </Link>{' '}
+            or through my social media accounts.
+          </p>
 
-        <p className="text-lg mt-4">
-          Hello, I&lsquo;m Nazif. I love working with Javascript technologies.
-          If you have a project, I would like to help you. You can contact me at{' '}
-          <Link
-            href="mailto:nazifcandurgut@icloud.com"
-            className="text-black font-medium hover:text-purple-600"
-          >
-            nazifcandurgut@icloud.com
-          </Link>{' '}
-          or through my social media accounts.
-        </p>
-
-        <div className="mt-4 flex items-center gap-4">
-          {SOCIAL_MEDIAS.map((social, index) => (
-            <Link key={index} href={social.url} target="_blank">
-              {social.icon}
-            </Link>
-          ))}
+          <div className="mt-4 flex items-center gap-4">
+            {SOCIAL_MEDIAS.map((social, index) => (
+              <Link key={index} href={social.url} target="_blank">
+                {social.icon}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
